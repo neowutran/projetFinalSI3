07513973810553d80a527be26522acf9b1aa3f0b
fabien <pinel.fabien@gmail.com>
2014-01-22 15:34:13 +0100

    register method

--- src/views/etat/Main.java
+++ src/views/etat/Main.java
@@ -5,7 +5,11 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+import config.Error;
+
 import model.Inventory;
+import model.MiniProjectException;
+import model.SaveLoad;
 import views.Command;
 import views.State;
 import views.View;
@@ -45,6 +49,28 @@ public class Main extends State {
         }
 
     }
+    /**
+     * Nouvelle méthode pour l'inscription d'un utilisateur
+     * 
+     * @param name nom du nouveau user
+     * @param id	id du nouveau user
+     * @param type	type du nouveau user
+     * @param password password du nouveau user
+     * @throws MiniProjectException 
+     */
+    @SuppressWarnings("unused")
+	private void register(String name, String id, String type, String password) throws MiniProjectException{
+    	switch (type) {
+        case SaveLoad.PERSON_TYPE_STUDENT:
+            new model.person.Borrower(name, id, SaveLoad.PERSON_TYPE_STUDENT, password);
+            break;
+        case SaveLoad.PERSON_TYPE_TEACHER:
+            new model.person.Borrower(name, id, SaveLoad.PERSON_TYPE_TEACHER, password);
+            break;
+        default:
+            throw new MiniProjectException(Error.CANNOT_CREATE_PERSON);
+    	}
+    }
 
     /*
      * (non-Javadoc)
@@ -61,8 +87,17 @@ public class Main extends State {
         args1.add( "password" );
         final Command command1 = new Command( "login", args1, this, "login",
                 "Se connecte a l'interface.\n\t Exemple d'utilisation:\n\t login 123 pass " );
+        
+        final List<String> args2 = new LinkedList<>( );
+        args2.add( "name" );
+        args2.add( "id" );
+        args2.add( "type" );
+        args2.add( "password" );
+        final Command command2 = new Command( "register", args2, this, "register",
+                "Inscription d'un nouvel utilisateur (type: teacher ou student)" );
 
         commands.add( command1 );
+        commands.add( command2 );
         commands.addAll( super.setCommands( ) );
 
         return commands;
